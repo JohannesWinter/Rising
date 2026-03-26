08a08edbc417d6498b99d763f6766edbf097f982
JohannesWinter <jvf.winter@web.de>
2026-03-26 22:50:52 +0100

    UpdatedBackgroundScroller_AddedBackgroundTextures

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -7,31 +7,43 @@ public class Background : MonoBehaviour
     public float speed;
     public GameObject backgroundObj;
     public float repeatingHeight;
+    public bool changeHeightRelative;
+    float layerStartY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        layerStartY = backgroundObj.transform.position.y;
+        if (changeHeightRelative == true)
+        {
+            foreach (Transform obj in backgroundObj.transform)
+            {
+                obj.transform.localPosition = new Vector3(obj.transform.localPosition.x, obj.transform.localPosition.y * (1 - speed), obj.transform.localPosition.z);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float newYVal = Manager.m.playerCamera.transform.position.y * speed;
-
-        if (repeatingHeight > 0)
+        if (Manager.m.playerCamera.transform.position.y >= layerStartY)
         {
-            float distanceToCam = Manager.m.playerCamera.transform.position.y - newYVal;
-            if (distanceToCam > repeatingHeight)
-            {
-                newYVal += Mathf.Floor(distanceToCam / repeatingHeight) * repeatingHeight;
-            }
-            else if (distanceToCam < -repeatingHeight)
-            {
-                newYVal -= Mathf.Floor(-distanceToCam / repeatingHeight) * repeatingHeight;
-            }
-        }
+            float newYVal = layerStartY + (Manager.m.playerCamera.transform.position.y - layerStartY) * speed;
 
-        Vector3 newPosition = new Vector3(backgroundObj.transform.position.x, newYVal, backgroundObj.transform.position.z);
-        backgroundObj.transform.position = newPosition;
+            if (repeatingHeight > 0)
+            {
+                float distanceToCam = Manager.m.playerCamera.transform.position.y - newYVal;
+                if (distanceToCam > repeatingHeight)
+                {
+                    newYVal += Mathf.Floor(distanceToCam / repeatingHeight) * repeatingHeight;
+                }
+                else if (distanceToCam < -repeatingHeight)
+                {
+                    newYVal -= Mathf.Floor(-distanceToCam / repeatingHeight) * repeatingHeight;
+                }
+            }
+
+            Vector3 newPosition = new Vector3(backgroundObj.transform.position.x, newYVal, backgroundObj.transform.position.z);
+            backgroundObj.transform.position = newPosition;
+        }
     }
 }
